23200d77f98b16cfdd51ce0a847661be783a1182
Felpzz2212 <felipearaujodealmeida3@gmail.com>
2025-04-10 20:37:15 -0300

    Alteracao estrutura json e criacao rota comercializacao

--- router/router.py
+++ router/router.py
@@ -1,13 +1,21 @@
 from flask import request, jsonify, Blueprint
-from services.scrapper import scrapping_producao
+from services.scrapper import scrapping_producao, scrapping_comercializacao
 
 vitvini_bp = Blueprint('vitivini', __name__, url_prefix='/vitivini')
 
 
 @vitvini_bp.route('/producao', methods=['GET'])
-def teste():
+def get_producao():
     ano = request.args.get('ano')
     if ano:
         return jsonify(scrapping_producao(ano))
     else:
         return jsonify(scrapping_producao())
+
+@vitvini_bp.route('/comercializacao', methods=['GET'])
+def get_comercializacao():
+    ano = request.args.get('ano')
+    if ano:
+        return jsonify(scrapping_comercializacao(ano))
+    else:
+        return jsonify(scrapping_comercializacao())
--- services/scrapper.py
+++ services/scrapper.py
@@ -25,11 +25,49 @@ def scrapping_producao(ano=2023):
             
             if len(td_item) > 0:
                 nome_item = td_item[0].text.strip()
-                resultado[nome_item] = []
+                resultado[nome_item] = {}
+                resultado[nome_item]['total'] = td_item[1].text.strip()
+                resultado[nome_item]['itens'] = []
             else:
                 td_subitem = linha.find_all('td', attrs={'class': 'tb_subitem'})
                 if nome_item and len(td_subitem) > 0:
-                    resultado[nome_item].append({f'{td_subitem[0].text.strip()}': f'{td_subitem[1].text.strip()}'})
+                    resultado[nome_item]['itens'].append({f'{td_subitem[0].text.strip()}': f'{td_subitem[1].text.strip()}'})
+                   
+    else:
+        resultado = {'message': 'Nao foi possivel recuperar informacoes do site'}
+    
+
+    return resultado
+
+def scrapping_comercializacao(ano=2023):
+
+    response = requests.get(f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_04')
+
+    if(response.status_code == 200):
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        print(soup)
+
+        tabela = soup.find('table', attrs={'class': 'tb_base tb_dados'})
+        tabela_body = tabela.find('tbody')
+        linhas = tabela_body.find_all('tr')
+        resultado = {} 
+
+
+        nome_item = None
+
+        for linha in linhas:
+            td_item = linha.find_all('td', attrs={'class': 'tb_item'})
+            
+            if len(td_item) > 0:
+                nome_item = td_item[0].text.strip()
+                resultado[nome_item] = {}
+                resultado[nome_item]['total'] = td_item[1].text.strip()
+                resultado[nome_item]['itens'] = []
+            else:
+                td_subitem = linha.find_all('td', attrs={'class': 'tb_subitem'})
+                if nome_item and len(td_subitem) > 0:
+                    resultado[nome_item]['itens'].append({f'{td_subitem[0].text.strip()}': f'{td_subitem[1].text.strip()}'})
                    
     else:
         resultado = {'message': 'Nao foi possivel recuperar informacoes do site'}
