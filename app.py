307a3c60ac16da4f10057fe1d52e816fd279c254
Felpzz2212 <felipearaujodealmeida3@gmail.com>
2025-04-12 11:36:06 -0300

    Alteracao Hsot

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ app.register_blueprint(vitvini_bp)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, port=port, host="0.0.0.0")
