edaba68bad880cfe7bbdaaf31a18663e175e7c1d
Felpzz2212 <felipearaujodealmeida3@gmail.com>
2025-04-12 10:45:48 -0300

    Criacao Rotas Processamento, Exportacao e Importacao

--- router/router.py
+++ router/router.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify, Blueprint
-from services.scrapper import scrapping_producao, scrapping_comercializacao
+from services.scrapper import scrapping_producao, scrapping_comercializacao, scrapping_processamento, scrapping_exportacao, scrapping_importacao
 
 vitvini_bp = Blueprint('vitivini', __name__, url_prefix='/vitivini')
 
@@ -19,3 +19,45 @@ def get_comercializacao():
         return jsonify(scrapping_comercializacao(ano))
     else:
         return jsonify(scrapping_comercializacao())
+    
+@vitvini_bp.route('/processamento', methods=['GET'])
+def get_processamento():
+    opt = request.args.get('opt')
+    ano = request.args.get('ano')
+    
+    if(ano and opt):
+        return jsonify(scrapping_processamento(ano, int(opt)))
+    elif(ano):
+        return jsonify(scrapping_processamento(ano))
+    elif(opt):
+        return jsonify(scrapping_processamento(opt=int(opt)))
+    else:
+        return jsonify(scrapping_processamento())
+
+@vitvini_bp.route('/exportacao', methods=['GET'])
+def get_exportacao():
+    opt = request.args.get('opt')
+    ano = request.args.get('ano')
+    
+    if(ano and opt):
+        return jsonify(scrapping_exportacao(ano, int(opt)))
+    elif(ano):
+        return jsonify(scrapping_exportacao(ano))
+    elif(opt):
+        return jsonify(scrapping_exportacao(opt=int(opt)))
+    else:
+        return jsonify(scrapping_exportacao())
+
+@vitvini_bp.route('/importacao', methods=['GET'])
+def get_importacao():
+    opt = request.args.get('opt')
+    ano = request.args.get('ano')
+    
+    if(ano and opt):
+        return jsonify(scrapping_importacao(ano, int(opt)))
+    elif(ano):
+        return jsonify(scrapping_importacao(ano))
+    elif(opt):
+        return jsonify(scrapping_importacao(opt=int(opt)))
+    else:
+        return jsonify(scrapping_importacao())
--- services/scrapper.py
+++ services/scrapper.py
@@ -10,8 +10,6 @@ def scrapping_producao(ano=2023):
     if(response.status_code == 200):
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        print(soup)
-
         tabela = soup.find('table', attrs={'class': 'tb_base tb_dados'})
         tabela_body = tabela.find('tbody')
         linhas = tabela_body.find_all('tr')
@@ -46,7 +44,46 @@ def scrapping_comercializacao(ano=2023):
     if(response.status_code == 200):
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        print(soup)
+        tabela = soup.find('table', attrs={'class': 'tb_base tb_dados'})
+        tabela_body = tabela.find('tbody')
+        linhas = tabela_body.find_all('tr')
+        resultado = {} 
+
+
+        nome_item = None
+
+        for linha in linhas:
+            td_item = linha.find_all('td', attrs={'class': 'tb_item'})
+            
+            if len(td_item) > 0:
+                nome_item = td_item[0].text.strip()
+                resultado[nome_item] = {}
+                resultado[nome_item]['total'] = td_item[1].text.strip()
+                resultado[nome_item]['itens'] = []
+            else:
+                td_subitem = linha.find_all('td', attrs={'class': 'tb_subitem'})
+                if nome_item and len(td_subitem) > 0:
+                    resultado[nome_item]['itens'].append({f'{td_subitem[0].text.strip()}': f'{td_subitem[1].text.strip()}'})
+                   
+    else:
+        resultado = {'message': 'Nao foi possivel recuperar informacoes do site'}
+    
+
+    return resultado
+
+def scrapping_processamento(ano=2023, opt=1):
+
+    opcoes = {
+        1: 'subopt_01',
+        2: 'subopt_02',
+        3: 'subopt_03',
+        4: 'subopt_04'
+    }
+
+    response = requests.get(f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_03&subopcao={opcoes[opt]}')
+
+    if(response.status_code == 200):
+        soup = BeautifulSoup(response.text, 'html.parser')
 
         tabela = soup.find('table', attrs={'class': 'tb_base tb_dados'})
         tabela_body = tabela.find('tbody')
@@ -68,6 +105,84 @@ def scrapping_comercializacao(ano=2023):
                 td_subitem = linha.find_all('td', attrs={'class': 'tb_subitem'})
                 if nome_item and len(td_subitem) > 0:
                     resultado[nome_item]['itens'].append({f'{td_subitem[0].text.strip()}': f'{td_subitem[1].text.strip()}'})
+                   
+    else:
+        resultado = {'message': 'Nao foi possivel recuperar informacoes do site'}
+    
+
+    return resultado
+
+def scrapping_importacao(ano=2024, opt=1):
+
+    opcoes = {
+        1: 'subopt_01',
+        2: 'subopt_02',
+        3: 'subopt_03',
+        4: 'subopt_04',
+        5: 'subopt_05'
+    }
+
+    response = requests.get(f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_05&subopcao={opcoes[opt]}')
+
+    if(response.status_code == 200):
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        tabela = soup.find('table', attrs={'class': 'tb_base tb_dados'})
+        tabela_body = tabela.find('tbody')
+        linhas = tabela_body.find_all('tr')
+        resultado = {} 
+
+        for linha in linhas:
+           campos = linha.find_all('td')
+           nome_pais = campos[0].text.strip()
+           quantidade = campos[1].text.strip()
+           valor = campos[2].text.strip()
+           resultado[nome_pais] = {}
+           resultado[nome_pais]['Quantidade'] = quantidade
+           resultado[nome_pais]['Valor'] = valor 
+                   
+    else:
+        resultado = {'message': 'Nao foi possivel recuperar informacoes do site'}
+    
+
+    return resultado
+
+def scrapping_exportacao(ano=2024, opt=1):
+
+    opcoes = {
+        1: 'subopt_01',
+        2: 'subopt_02',
+        3: 'subopt_03',
+        4: 'subopt_04'
+    }
+
+    response = requests.get(f'http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao=opt_06&subopcao={opcoes[opt]}')
+
+    if(response.status_code == 200):
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        tabela = soup.find('table', attrs={'class': 'tb_base tb_dados'})
+        tabela_body = tabela.find('tbody')
+        linhas = tabela_body.find_all('tr')
+        resultado = {} 
+
+
+        if(response.status_code == 200):
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            tabela = soup.find('table', attrs={'class': 'tb_base tb_dados'})
+            tabela_body = tabela.find('tbody')
+            linhas = tabela_body.find_all('tr')
+            resultado = {} 
+
+            for linha in linhas:
+                campos = linha.find_all('td')
+                nome_pais = campos[0].text.strip()
+                quantidade = campos[1].text.strip()
+                valor = campos[2].text.strip()
+                resultado[nome_pais] = {}
+                resultado[nome_pais]['Quantidade'] = quantidade
+                resultado[nome_pais]['Valor'] = valor 
                    
     else:
         resultado = {'message': 'Nao foi possivel recuperar informacoes do site'}
